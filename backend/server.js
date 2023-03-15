83e83bceafa436ce032ea53ae2526004cce9ea81
hari <harikn77@gmail.com>
2023-03-15 21:42:40 +0530

    Add env configuration to backend

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,17 @@ const bodyParser = require('body-parser');
 const { Client } = require('elasticsearch');
 const amqp = require('amqplib');
 const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
-// const uri = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const isLocal = false; // change when using in local
-let host = "192.168.1.189"
-if(isLocal) {
-  host = "localhost"
-}
-
-const uri = `mongodb://${host}:27017/mydatabase`;
+const uri = process.env.MONGO_URI;
 const mongo_client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
-const ELASTICSEARCH_HOST = `http://${host}:9200`;
-const RABBITMQ_HOST = `amqp://${host}`;
+const ELASTICSEARCH_HOST = process.env.ELASTICSEARCH_HOST
+const RABBITMQ_HOST = `amqp://${process.env.RM_HOST}`;
 
 const elasticsearchClient = new Client({
   host: ELASTICSEARCH_HOST
@@ -33,21 +28,21 @@ app.get('/', (req, res) => {
 // endpoint to search for dataset
 app.get('/search/', async (req, res) => {
   console.log("Inside")
-  const { datasetName, query, fields, sorting, pagination, facets } = req.query; 
+  const { datasetName, query, fields, sorting, pagination, facets } = req.query;
   // Query Parameters
   // queryText - string value that will be used for querying in the indexed data. 
   // pagination - maximum number
   // const searchQuery = req.params.searchText;
-   elasticsearchClient.search({
+  elasticsearchClient.search({
     index: datasetName,
     body: {
       "query": {
-          "match": {
-            "_all": query
-          }
+        "match": {
+          "_all": query
+        }
       }
     }
-  }).then((searchResponse)=> {
+  }).then((searchResponse) => {
     res.send(searchResponse.hits.hits);
   }).catch((e) => {
     console.log(e)
@@ -81,15 +76,15 @@ const update_mongo_status = async (dataset_name) => {
   }
 };
 
-const bulk_index = async (indexing_data,index_name) => {
+const bulk_index = async (indexing_data, index_name) => {
   const body = indexing_data.reduce((bulkRequestBody, doc) => {
-      bulkRequestBody += JSON.stringify({ index: { _index: index_name } }) + '\n';
-      bulkRequestBody += JSON.stringify(doc) + '\n';
-      return bulkRequestBody;
+    bulkRequestBody += JSON.stringify({ index: { _index: index_name } }) + '\n';
+    bulkRequestBody += JSON.stringify(doc) + '\n';
+    return bulkRequestBody;
   }, '');
 
   const response = await elasticsearchClient.bulk({
-      body: body
+    body: body
   });
   console.log('indexing completed');
   update_mongo_status(index_name);
@@ -110,8 +105,8 @@ async function consumeQueue() {
     channel.consume(
       queue,
       async (msg) => {
-        const metadata = JSON.parse(msg.content.toString());        
-        
+        const metadata = JSON.parse(msg.content.toString());
+
         console.log(`Received metadata for dataset ${metadata.dataset_name}`);
         try {
           const index = metadata.dataset_name;
